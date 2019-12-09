fb67079ccc7f704160bbffc3b76c6cba023c0035
Arafath Baig <arafath.moghul@gmail.com>
2019-12-09 11:52:25 +0530

    Parse query commited

--- app/src/main/java/arafath/myappcom/instagram_clone20/MainActivity.java
+++ app/src/main/java/arafath/myappcom/instagram_clone20/MainActivity.java
@@ -6,36 +6,76 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
 
-    Button btn;
+    Button btn,btn2;
     EditText editText, editText2, editText3, editText4;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btn = findViewById(R.id.button);
+        btn2 = findViewById(R.id.btnGetData);
         btn.setOnClickListener(MainActivity.this);
 
-
+        textView = findViewById(R.id.textView);
         editText = findViewById(R.id.editText);
         editText2 = findViewById(R.id.editText2);
         editText3 = findViewById(R.id.editText3);
         editText4 = findViewById(R.id.editText4);
 
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("KickBoxer");
+                query.getInBackground("dLIYHCQtE0", new GetCallback<ParseObject>() {
+                    @Override
+                    public void done(ParseObject object, ParseException e) {
+                        if(object != null && e == null){
+                            textView.setText(object.get("punch_power")+" ");
+                        }
+                    }
+                });
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("KickBoxer");
+                query.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(List<ParseObject> objects, ParseException e) {
+
+                        if (objects.size() > 0) {
+                            FancyToast.makeText(MainActivity.this, " saved Succesfully", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
 
     }
+
 
     @Override
     public void onClick(View v) {
