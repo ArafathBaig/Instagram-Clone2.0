5fcf56b0e2c4a9596b764d220fe31fc4797ed9da
Arafath Baig <arafath.moghul@gmail.com>
2019-12-12 16:47:46 +0530

    share tab commit

--- app/src/main/java/arafath/myappcom/instagram_clone20/SharePicTab.java
+++ app/src/main/java/arafath/myappcom/instagram_clone20/SharePicTab.java
@@ -1,20 +1,30 @@
 package arafath.myappcom.instagram_clone20;
 
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SharePicTab extends Fragment {
-
+public class SharePicTab extends Fragment implements View.OnClickListener {
+        private ImageView imageView;
+        private EditText editDesc;
+        private Button btnshare;
 
     public SharePicTab() {
         // Required empty public constructor
@@ -25,7 +35,50 @@ public class SharePicTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_share_pic_tab, container, false);
+        View view =  inflater.inflate(R.layout.fragment_share_pic_tab, container, false);
+
+        imageView = view.findViewById(R.id.imageView);
+        editDesc = view.findViewById(R.id.editDescription);
+        btnshare = view.findViewById(R.id.buttonSend);
+
+        imageView.setOnClickListener(SharePicTab.this);
+        btnshare.setOnClickListener(SharePicTab.this);
+
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.imageView:
+                if(android.os.Build.VERSION.SDK_INT >= 23 &&
+                        ActivityCompat.checkSelfPermission(getContext(),
+                                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+
+                    requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1000);
+                }else{
+                    getChosenImage();
+                }
+                break;
+
+            case R.id.buttonSend:
+                break;
+        }
+    }
+
+    private void getChosenImage() {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == 1000){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getChosenImage();
+            }
+        }
+    }
 }
