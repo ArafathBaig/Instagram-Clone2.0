83269e7777103761148cceef46df735370a067fb
Arafath Baig <arafath.moghul@gmail.com>
2019-12-12 12:49:14 +0530

    fragment tabs commit

--- app/src/main/java/arafath/myappcom/instagram_clone20/SocialMediaActivity.java
+++ app/src/main/java/arafath/myappcom/instagram_clone20/SocialMediaActivity.java
@@ -1,14 +1,39 @@
 package arafath.myappcom.instagram_clone20;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
+
 public class SocialMediaActivity extends AppCompatActivity {
+
+    private androidx.appcompat.widget.Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabAdapter tabAdapter;
+    private TabLayout tabLayout;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
+
+        setTitle("User Feed");
+
+        toolbar = findViewById(R.id.mytoolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabAdapter = new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager,false);
     }
 }
